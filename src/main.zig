37b9f34b0236fbb889795a0a452793e020dab75b
slinky55 <larry.mason@ufl.edu>
2024-10-11 08:25:50 -0400

    initial commit

---
+++ src/main.zig
@@ -0,0 +1,10 @@
+//! By convention, main.zig is where your main function lives in the case that
+//! you are building an executable. If you are making a library, the convention
+//! is to delete this file and start with root.zig instead.
+const std = @import("std");
+
+const print = std.debug.print;
+
+pub fn main() !void {
+    print("Hello, World!", .{});
+}
